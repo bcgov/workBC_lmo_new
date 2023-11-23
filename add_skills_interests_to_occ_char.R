1b4245c576748dff528b545ec4f5789d9e3faf31
Rich-P-Martin <richard.martin@gov.bc.ca>
2023-11-23 10:21:16 -0800

    mostly documentation...

--- add_skills_interests_to_occ_char.R
+++ add_skills_interests_to_occ_char.R
@@ -1,8 +1,16 @@
+#' This script adds skills and interests to the file occupational characteristics
+#' Inputs: Excel files
+#'
+#' "Occupational Characteristics based..."
+#' "Occupational interest..."
+#' "Top skills..."
+#libraries--------------
 library(readxl)
 library(here)
 library(tidyverse)
 library(openxlsx)
-
+library(conflicted)
+#functions-------------------------------
 get_skill_names <- function(tbbl){
   on_own <- tbbl$`Skills & Competencies`
   together <- paste(on_own, collapse = ", ")
@@ -10,7 +18,6 @@ get_skill_names <- function(tbbl){
   colnames(temp) <- c("Skill1", "Skill2", "Skill3", "Skills: Top 3")
   temp
 }
-
 get_interests_names <- function(tbbl){
   tbbl <- tbbl|>
     na.omit()
@@ -21,8 +28,8 @@ get_interests_names <- function(tbbl){
   colnames(temp) <- c("Interest1", "Interest2", "Interest3", "Interests")
   temp
 }
-
-skills_df <- read_excel(here("data","Top skills by NOC2021 occupations.xlsx"))|>
+#Read in the data------------------------------
+skills_df <- read_excel(here("data",list.files(here("data"), pattern = "Top skills")))|>
   group_by(NOC=NOC2021)|>
   mutate(NOC=paste0("#",NOC))|>
   slice_max(`Importance Score`, n=3, with_ties = FALSE)|>
@@ -31,7 +38,9 @@ skills_df <- read_excel(here("data","Top skills by NOC2021 occupations.xlsx"))|>
   select(-data)|>
   unnest(skills)
 
-interests_df <- read_excel(here("data","Occupational interest by NOC2021 occupation.xlsx"))|>
+interests_df <- read_excel(here("data",
+                                list.files(here("data"),
+                                           pattern = "Occupational interest")))|>
   select(NOC=`NOC 2021`, `Occupational Interest`)|>
   mutate(NOC=paste0("#",NOC))|>
   group_by(NOC)|>
@@ -47,7 +56,11 @@ occ_char <- read_excel(here("data",
   left_join(skills_df)|>
   left_join(interests_df)
 
-openxlsx::write.xlsx(occ_char, here("data","Occupational Characteristics with skills and interests.xlsx"), asTable = TRUE)
+#write to disk----------------------------------
+openxlsx::write.xlsx(occ_char,
+                     here("data",
+                          "Occupational Characteristics with skills and interests.xlsx"),
+                     asTable = TRUE)
 
 
 
